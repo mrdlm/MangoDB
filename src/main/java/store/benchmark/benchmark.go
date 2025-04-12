83893e7f0e49733e459718fc67f6633c238f1942
mrdlm <mridul.mohan.m@gmail.com>
2025-04-12 16:24:44 +0530

    #1: make mangodbserver multithreaded

--- src/main/java/store/benchmark/benchmark.go
+++ src/main/java/store/benchmark/benchmark.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	_ "index/suffixarray"
 	"math/rand"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -24,18 +26,90 @@ func generateRandomString(length int) string {
 func main() {
 
 	fmt.Println("Starting benchmark with random keys and values...\n\n")
+	// runSingleConnectionBenchmark()
+	runMultiConnectionBenchmark(50, 100000) // 10 connections, 1000 requests per connection
+}
+
+func runMultiConnectionBenchmark(numConnections, numRequestsPerConnection int) {
+	fmt.Printf("Starting multi-connection benchmark with %d connection, and %d requests per connection\n\n",
+		numConnections, numRequestsPerConnection)
+
+	var wg sync.WaitGroup
+	start := time.Now()
+
+	var failedConnection int
+	var countMutex sync.Mutex
+
+	for i := 0; i < numConnections; i++ {
+		wg.Add(1)
+
+		go func(connID int) {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", addr)
+			if err != nil {
+				fmt.Println("Connection failed:", err)
+
+				// thread safe counting
+				countMutex.Lock()
+				failedConnection++
+				countMutex.Unlock()
+
+				return
+			}
+
+			defer conn.Close()
+
+			writer := bufio.NewWriter(conn)
+			reader := bufio.NewReader(conn)
+
+			for j := 0; j < numRequestsPerConnection; j++ {
+				key := generateRandomString(5)
+				value := generateRandomString(100)
+
+				cmd := fmt.Sprintf("PUT %s %s\n", key, value)
+				_, err := writer.WriteString(cmd)
+
+				if err != nil {
+					fmt.Println("Write failed:", err)
+					return
+				}
+
+				err = writer.Flush()
+				if err != nil {
+					return
+				}
+
+				_, err = reader.ReadString('\n')
+				if err != nil {
+					fmt.Println("Read failed:", err)
+					return
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	duration := time.Since(start)
+
+	fmt.Printf("Benchmarking completed in %s\n", time.Since(start))
+	fmt.Printf("Write Throughput: %v ops/s\n\n",
+		float64(numRequestsPerConnection*(numConnections-failedConnection))/duration.Seconds())
+}
+
+func runSingleConnectionBenchmark() {
 	conn, err := net.Dial("tcp", addr)
+
 	if err != nil {
 		panic(err)
 	}
+
+	defer conn.Close()
 
 	writer := bufio.NewWriter(conn)
 	reader := bufio.NewReader(conn)
 
 	runWriteBenchmark(writer, reader)
-	// runReadBenchmark(writer, reader)
-
-	defer conn.Close()
 }
 
 func runWriteBenchmark(writer *bufio.Writer, reader *bufio.Reader) {
